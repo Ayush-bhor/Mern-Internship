a54a0600bde53c7345afc6d116f20bf48de2628b
AyushBhor <ayush.bhor05@gmail.com>
2026-06-29 21:56:32 +0530

    class task

---
+++ 29-June/cart.mjs
@@ -0,0 +1,18 @@
+
+export function product() {
+    return {
+        pnameModel: "Iphone 14",
+        price: 120000,
+        color: "Black",
+        ram: "8GB",
+        rom: "128GB",
+        camera: "12MP",
+        battery: "4000mAh"
+    };
+}
+export default function a() {
+    return {
+        pnameModel: "Iphones",
+        
+    };
+}
---
+++ 29-June/product.js
@@ -0,0 +1,4 @@
+import { product } from './product.js'
+import a from './product.js'
+console.log(product());
+console.log(a());
