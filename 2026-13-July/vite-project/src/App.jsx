94e6af1036aa533b1c0ba24f87c21e13b3600cda
AyushBhor <ayush.bhor05@gmail.com>
2026-07-13 12:13:09 +0530

    useeffect

---
+++ 2026-13-July/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UseEffect from './useeffect'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <UseEffect />
+    </div>
+  )
+}
+
+export default App
